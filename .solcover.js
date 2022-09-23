48a50f2e4f30dd762293487cb7a4a1dca36ab15f
Sebastian Kanz <sebastian.kanz@maibornwolff.de>
2022-09-23 11:19:32 +0200

    fix ganche cli

--- .solcover.js
+++ .solcover.js
@@ -1,4 +1,5 @@
 module.exports = {
+  client: require("ganache-cli"), // Will load the outermost ganache-cli in node_modules
   providerOptions: {
     mnemonic:
       "heart rocket ripple ritual arrow group visa execute possible castle balcony worry",
